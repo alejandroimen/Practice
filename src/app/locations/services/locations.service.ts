a6be7de624323cbd33e68d5422f09082933a263d
Kevin <moralesperezkevin7@gmail.com>
2024-11-22 13:31:52 -0600

    Consumiendo la api de locaciones

--- src/app/locations/services/locations.service.ts
+++ src/app/locations/services/locations.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ILocationsComplete } from '../interfaces/ilocations-complete';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private url: string = 'https://rickandmortyapi.com/api';
+
+  getAllLocations(): Observable<ILocationsComplete> {
+    return this.http.get<ILocationsComplete> (`${this.url}/location`);
+  }
 }
